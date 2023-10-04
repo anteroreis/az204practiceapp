1b57fb0be63df9433593f7d66e54010593061257
Antero Reis <antero.reis@innofactor.com>
2023-10-04 14:21:22 +0300

    Write logs

--- az204practiceapp/Pages/Index.cshtml.cs
+++ az204practiceapp/Pages/Index.cshtml.cs
@@ -14,7 +14,30 @@ namespace az204practiceapp.Pages
 
         public void OnGet()
         {
+            var remoteHost = this.HttpContext.GetServerVariable("REMOTE_HOST");
+            var remoteAddress = this.HttpContext.GetServerVariable("REMOTE_ADDR");
+            var msg = $"OnGet remote host {remoteHost} remote address {remoteAddress}";
 
+            System.Diagnostics.Trace.TraceInformation(msg);
+        }
+
+        public void OnPost()
+        {
+
+            var remoteHost = this.HttpContext.GetServerVariable("REMOTE_HOST");
+            var remoteAddress = this.HttpContext.GetServerVariable("REMOTE_ADDR");
+            var msg = $"OnPost remote host {remoteHost} remote address {remoteAddress}";
+
+            System.Diagnostics.Trace.TraceInformation(msg);
+
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError(ex.Message);
+            }
         }
     }
 }
